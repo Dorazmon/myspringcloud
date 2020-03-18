6e6e2e837f57010b07fa774e6f1f1d28d40d3c61
zhang <377207104@qq.com>
2020-03-18 17:58:58 +0800

    ribbon增加了熔断器

--- service-ribbon/src/main/java/com/zhang/service/HelloService.java
+++ service-ribbon/src/main/java/com/zhang/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.zhang.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +10,12 @@ public class HelloService implements IHelloService{
   @Autowired
   private RestTemplate restTemplate;
 
-  @Override
+  @HystrixCommand(fallbackMethod = "hiError")
   public String hiService(String name) {
     return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+  }
+
+  public String hiError(String name) {
+    return "hi,"+name+",对不起连接不上";
   }
 }
