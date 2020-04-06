ba5ac001f21bb78bad6a98078b7ef855ca5fda36
zhanggan <377207104@qq.com>
2020-04-06 23:08:50 +0800

    增加OAuth2.0权限管理

--- service-feign/src/main/java/com/zhang/control/FeignController.java
+++ service-feign/src/main/java/com/zhang/control/FeignController.java
@@ -2,6 +2,7 @@ package com.zhang.control;
 
 import com.zhang.service.IFeignCall;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class FeignController {
   @Autowired
   private IFeignCall FeignCallImpl;
   @GetMapping(value = "/hi")
+  @PreAuthorize("hasAuthority('p3')")
   public String sayHi(@RequestParam String name) {
     return FeignCallImpl.sayHiFromClientOne( name )+"---feign调用的";
   }
