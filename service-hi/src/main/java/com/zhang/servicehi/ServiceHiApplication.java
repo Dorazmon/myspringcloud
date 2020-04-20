18ccc0e6c47597be7f1090909eaee9f24a920079
zhanggan <377207104@qq.com>
2020-04-20 15:42:09 +0800

    增加日志配置logback

--- service-hi/src/main/java/com/zhang/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/zhang/servicehi/ServiceHiApplication.java
@@ -1,5 +1,6 @@
 package com.zhang.servicehi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@Slf4j
 public class ServiceHiApplication {
   public static void main(String[] args) {
     SpringApplication.run( ServiceHiApplication.class, args );
@@ -21,6 +23,10 @@ public class ServiceHiApplication {
 
   @RequestMapping("/hi")
   public String home(@RequestParam(value = "name") String name) {
+    log.debug("{debug日志}");
+    log.info("{info日志}");
+    log.warn("{warn日志}");
+    log.error("{error日志}");
     return "名字"+name+" ,端口号:" + port;
   }
 
